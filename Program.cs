94151931a66fdc4083ecf8e72be92a9a0438a41a
seungjun123 <80227755+seungjun123@users.noreply.github.com>
2021-03-22 22:25:40 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,24 @@
 ﻿using System;
-
-
-namespace namespace_StringConcat
+namespace A007_ConsoleWriteLine
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string userName = "bikang";
-            string date = DateTime.Today.ToShortDateString();
-            string strPlus = "Hello " + userName + ". Today is " + date + ".";
-            Console.WriteLine(strPlus);
-            string strFormat = String.Format("Hello {0}. Today is {1}.", userName, date);
-            Console.WriteLine(strFormat);
-            string strInterpolation = $"Hello {userName}. Today is {date}.";
-            System.Console.WriteLine(strInterpolation);
-            //string name = "bikang";
-            //int age = 12;
-            //Console.WriteLine($"{name} is {age} year{(age == 1 ? "" : "s")} old.");
-            //Console.WriteLine(name + " is " + age + " year" + (age == 1 ? "" : "s") + " old.");
-            string strConcat = String.Concat("Hello ", userName, ". Today is ", date, ".");
-            Console.WriteLine(strConcat);
-            string[] animals = { "mouse", "cow", "tiger", "rabbit", "dragon" };
-            string s = String.Concat(animals);
-            Console.WriteLine(s);
-            s = String.Join(", ", animals);
+            bool b = true;
+            char c = 'A';
+            decimal d = 1.234m; // m은 decimal 형의 접미사
+            double dd = 1.23456789;
+            float f = 1.23456789f; // f는 float 형의 접미사
+            int i = 1234;
+            string s = "Hello";
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(dd);
+            Console.WriteLine(f);
+            Console.WriteLine(i);
             Console.WriteLine(s);
         }
     }
 }
-      
-
